c58117bc4d977f9d6e3493dc17c3dd6b232f7830
VinicinBraga <vinicius@bragamatta.com.br>
2022-11-08 16:16:29 -0300

    Server structure

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import * as dotenv from "dotenv";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+const port = 3000;
+
+dotenv.config();
+
+app.use("/posts", postRoutes); //all in postRouts will begining with /posts
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+mongoose
+  .connect(
+    `mongodb+srv://user:${process.env.PASSWORD}@memories-data.7y7emym.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() =>
+    app.listen(port, () =>
+      console.log(`Example app listening on port ${port}!`)
+    )
+  )
+  .catch((error) => console.log(error));
