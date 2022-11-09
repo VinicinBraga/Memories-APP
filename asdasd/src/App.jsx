bca4d21b2f939582321932272d09d50a336dfd06
VinicinBraga <vinicius@bragamatta.com.br>
2022-11-09 11:22:37 -0300

    Main Css

--- asdasd/src/App.jsx
+++ asdasd/src/App.jsx
@@ -2,35 +2,27 @@ import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
 import memories from "./images/memories.png";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <Container maxWidth="sm">
-        <AppBar position="static" color="inherit">
-          <Typography variant="h2" align="center">
-            Memories
-          </Typography>
-          <img src={memories} alt="memories" height="340" />
-        </AppBar>
-        <Grow in>
-          <Container>
-            <Grid
-              container
-              justifyContent="space-between"
-              alignItems="stretch"
-              spacing={3}
-            >
-              <Grid item xs={12} sm={7}>
-                <Posts />
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <Form />
-              </Grid>
-            </Grid>
-          </Container>
-        </Grow>
-      </Container>
+    <div className="container">
+      <div className="appBar">
+        <h2 className="heading">Memories</h2>
+        <img className="image" src={memories} alt="memories" />
+      </div>
+      <div>
+        <div className="container-grid">
+          <div className="grid">
+            <div className="posts-grid">
+              <Posts />
+            </div>
+            <div className="form-grid">
+              <Form />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- asdasd/src/components/Posts/Posts.jsx
+++ asdasd/src/components/Posts/Posts.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Post from "./Post/Post.jsx";
+import "./style.css";
 
 function Posts() {
   return (
-    <>
+    <div className="posts-container">
       <h1>Posts</h1>
       <Post />
       <Post />
-    </>
+    </div>
   );
 }
 
